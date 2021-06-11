2040266fa8c9900e99c211ab1768154b20a7dd6e
tushar0707 <tusharrrr77@gmail.com>
2021-06-11 14:45:48 +0530

    commit

--- main.js
+++ main.js
@@ -15,3 +15,50 @@ name2 = document.getElementById("email").value;
 name3 = document.getElementById("uname").value;
 console.log(name1,name2,name3);
   }
+
+
+  /*Downloaded from https://www.codeseek.co/ovdojoey/css-gift-opening-pgozGX */
+
+var to = 'Suprise!';
+var gift_url = 'http://www.amazon.com/gp/product/B00X4WHP5E/ref=ods_xs_ae_shurl?tag=googhydr-20&hvadid=74652194316&hvpos=1t1&hvexid=&hvnetw=g&hvrand=7560558939874219351&hvpone=&hvptwo=&hvqmt=b&hvdev=c&ref=pd_sl_2vatdqwe3p_b';
+var gift_image_url = 'https://media-exp3.licdn.com/dms/image/C560BAQHTvZwCx4p2Qg/company-logo_200_200/0/1612205615891?e=2159024400&v=beta&t=J9qbDyzP2uv1lE1Xb_ieBaWwgeT-u52Mf-4ACuHP_p8';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
